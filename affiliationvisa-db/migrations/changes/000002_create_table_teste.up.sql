01ed73904be0d8531e707da15c39622064566758
Fabiano Furtado de Melo <fabianofm1975@gmail.com>
2023-04-20 17:53:11 -0300

    ajuste

--- affiliationvisa-db/migrations/changes/000002_create_table_teste.up.sql
+++ affiliationvisa-db/migrations/changes/000002_create_table_teste.up.sql
@@ -1,12 +1,4 @@
 CREATE TABLE IF NOT EXISTS teste
 (
-    id uuid NOT NULL CONSTRAINT onboarding_pkey PRIMARY KEY,
-    correlation_id uuid NOT NULL,
-    invoice_id uuid NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE default NOW() NOT NULL,
-    cart_data jsonb NOT NULL,
-    data jsonb NOT NULL,
-    document_number VARCHAR(14) NOT NULL,
-    payment_link VARCHAR(100) NOT NULL,
-    risk_analysis_key uuid NULL
+
 );
