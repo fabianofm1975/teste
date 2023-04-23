d0418fddb8c379df993973614a8f5268c6377930
Fabiano Furtado de Melo <fabianofm1975@gmail.com>
2023-04-23 13:09:24 -0300

    USERS

--- affiliationvisa-db/users/affiliationvisa_app_user.sql
+++ affiliationvisa-db/users/affiliationvisa_app_user.sql
@@ -1,10 +1,3 @@
 CREATE USER "AffiliationVisaAppUser" WITH ENCRYPTED PASSWORD 'Stone@@2023!';
 
 GRANT CONNECT ON DATABASE "AffiliationVisaDB" TO "AffiliationVisaAppUser";
-
-GRANT USAGE ON SCHEMA public TO "AffiliationVisaAppUser";
-
-GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO "AffiliationVisaAppUser";
-
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO "AffiliationVisaAppUser";
-
