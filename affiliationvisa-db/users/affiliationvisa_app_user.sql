d4969582cf1f0aed737ec91f7ba642a372524c6f
Fabiano Furtado de Melo <fabianofm1975@gmail.com>
2023-04-23 14:00:30 -0300

    ajustes

--- affiliationvisa-db/users/affiliationvisa_app_user.sql
+++ affiliationvisa-db/users/affiliationvisa_app_user.sql
@@ -1,7 +1,7 @@
 CREATE USER "AffiliationVisaAppUser" WITH ENCRYPTED PASSWORD 'Stone@@2023!';
 GRANT CONNECT ON DATABASE "AffiliationVisaDB" TO "AffiliationVisaAppUser";
 
--- Alterando default privileges
+-- Alterando default privileges EXECUTAR NO BANCO "AffiliationVisaDB"
 
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO "AffiliationVisaAppUser";
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT,USAGE ON SEQUENCES TO "AffiliationVisaAppUser";
