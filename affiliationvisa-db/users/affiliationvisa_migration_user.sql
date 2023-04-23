ae0ad64c8755fc077c5c7192da5350cd8cccf823
Fabiano Furtado de Melo <fabianofm1975@gmail.com>
2023-04-23 14:44:25 -0300

    ajustes

--- affiliationvisa-db/users/affiliationvisa_migration_user.sql
+++ affiliationvisa-db/users/affiliationvisa_migration_user.sql
@@ -1,9 +1,8 @@
 CREATE USER migrationappuser WITH ENCRYPTED PASSWORD 'Stone@@2023!';
-
 GRANT CONNECT ON DATABASE "AffiliationVisaDB" TO migrationappuser;
 
+-- Executar no banco AffiliationVisaDB
+
 GRANT USAGE ON SCHEMA public TO migrationappuser;
-
 GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO migrationappuser;
-
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO migrationappuser;
