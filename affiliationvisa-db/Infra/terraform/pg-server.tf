15106d6b7b3d811067f82c549609356aea965904
Fabiano Furtado de Melo <fabianofm1975@gmail.com>
2023-04-24 10:41:20 -0300

    rg

--- affiliationvisa-db/Infra/terraform/pg-server.tf
+++ affiliationvisa-db/Infra/terraform/pg-server.tf
@@ -1,7 +1,12 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "Automacao"
+  location = "eastus"
+}
+
 resource "azurerm_postgresql_flexible_server" "AffiliationVisa" {
   name                          = "affiliationvisa-db"
-  resource_group_name           = "Automacao"
-  location                      = "eastus"
+  resource_group_name           = azurerm_resource_group.rg.name
+  location                      = azurerm_resource_group.rg.location
   version                       = "12"
   administrator_login           = "psqladminafl"
   administrator_password        = "Stone@@2023!"
