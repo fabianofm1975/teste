1919f4f450029b5b935a77759c4875ae3d3c7918
Fabiano Furtado de Melo <fabianofm1975@gmail.com>
2023-04-23 21:22:17 -0300

    tf

--- affiliationvisa-db/Infra/terraform/main.tf
+++ affiliationvisa-db/Infra/terraform/main.tf
@@ -2,23 +2,22 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
+      version = "3.36.0"
     }
-
-provider "azurerm" {
-  features {}
-}
-
-backend "azurerm" {
-        resource_group_name  = "rg_terraform"
+  }
+  backend "azurerm" {
+        resource_group_name  = "rg"
         storage_account_name = "fabianofm1975"
         container_name       = "terraform"
         key                  = "terraform.tfstate"
     }
 }
 
-resource "azurerm_resource_group" "rg_terraform" {
-  name     = "Terraform"
-  location = "eastus"
+provider "azurerm" {
+  features {}
 }
 
-  }
+resource "azurerm_resource_group" "rg" {
+  name     = "Automacao"
+  location = "eastus"
+}
--- affiliationvisa-db/Infra/terraform/pg-server.tf
+++ affiliationvisa-db/Infra/terraform/pg-server.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "rg_db" {
+resource "azurerm_resource_group" "rg" {
   name     = "Automacao"
   location = "eastus"
 }
