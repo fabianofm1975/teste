0247648568057d39ef6620ece98eaf9418511ee8
Fabiano Furtado de Melo <fabianofm1975@gmail.com>
2023-05-18 14:20:46 -0300

    teste migration

--- affiliationvisa-db/users/affiliationvisa_migration_user.sql
+++ affiliationvisa-db/users/affiliationvisa_migration_user.sql
@@ -1,15 +1,15 @@
--- Executar com usuário admin da cloud no banco POSTGRES
-
--- Removendo privilégios herdados da role PUBLIC
+-- Removendo privilégios herdados da role PUBLIC. Deve ser executado, com usuário admin da cloud ou postgres
+-- em todos os bancos existentes e em todo novo banco criado.
 
 REVOKE ALL ON SCHEMA public FROM public;
 
 -- Criando o usuário de migration
 
-CREATE USER migrationappuser WITH ENCRYPTED PASSWORD 'Stone@@2023!';
-GRANT CONNECT ON DATABASE "AffiliationVisaDB" TO migrationappuser;
+CREATE USER migrationappuser WITH ENCRYPTED PASSWORD 'Stone@2023!';
+GRANT CONNECT ON DATABASE "teste-db" TO migrationappuser;
 
--- Executar no banco AffiliationVisaDB logado com usuário admin da cloud
+-- Executar no banco "teste-db" logado com usuário admin da cloud ou postgres
 
+REVOKE ALL ON SCHEMA public FROM public;
 GRANT ALL ON SCHEMA public TO migrationappuser;
 
