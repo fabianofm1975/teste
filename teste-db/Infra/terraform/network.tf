66f92fc123c189fd0f9891d8ce1ce987a74840f9
Fabiano Furtado de Melo <fabianofm1975@gmail.com>
2023-05-22 11:08:03 -0300

    teste

--- teste-db/Infra/terraform/network.tf
+++ teste-db/Infra/terraform/network.tf
@@ -1,5 +1,5 @@
 resource "azurerm_postgresql_flexible_server_firewall_rule" "testedb-fw" {
-  name             = "Open-fw"
+  name             = "Open-all"
   server_id        = azurerm_postgresql_flexible_server.testedb.id
   start_ip_address = "0.0.0.0"
   end_ip_address   = "255.255.255.255"
