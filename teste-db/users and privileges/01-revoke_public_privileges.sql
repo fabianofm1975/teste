3d69e13b979ba6b2d9a24cfc15934302123fbc77
Fabiano Furtado de Melo <fabianofm1975@gmail.com>
2023-05-22 14:04:27 -0300

    users

--- teste-db/users and privileges/01-revoke_public_privileges.sql
+++ teste-db/users and privileges/01-revoke_public_privileges.sql
@@ -1,4 +1,6 @@
 -- Removendo privilégios herdados da role PUBLIC. 
--- Deve ser executado com usuário ADMIN da cloud ou POSTGRES, em TODOS OS BANCOS existentes e em TODO NOVO BANCO criado.
+-- Deve ser executado com usuário ADMIN da cloud ou POSTGRES no NOVO BANCO criado.
 
 REVOKE ALL ON SCHEMA public FROM public;
+REVOKE ALL ON DATABASE POSTGRES FROM public;
+
